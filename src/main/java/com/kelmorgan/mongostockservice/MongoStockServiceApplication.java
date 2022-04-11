90ef4e1c8e376d087e6ee27f4582e3e6979ad5c2
kelmorgan <kelmorgan18@gmail.com>
2022-04-11 14:16:05 +0100

    First commit

---
+++ src/main/java/com/kelmorgan/mongostockservice/MongoStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kelmorgan.mongostockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoStockServiceApplication.class, args);
+	}
+
+}
